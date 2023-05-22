d7ad72cb9212397f05573404951e7606aa939d51
Trung Nguyen <thientuquangtrung@gmail.com>
2023-05-22 23:22:59 +0700

    fix file structure

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,7 +1,7 @@
 // Layouts
 import DefaultLayout from '../layouts/DefaultLayout/DefaultLayout';
-
 import SellerLayout from '../layouts/SellerLayout/SellerLayout';
+import AuthLayout from '../layouts/AuthLayout/Authlayout';
 
 // Pages
 import Home from '../pages/Home/Home';
@@ -12,14 +12,15 @@ import NewProduct from '../pages/NewProduct/NewProduct';
 import ListProduct from '../pages/ListProduct/ListProduct';
 import ProductDetail from '../pages/ProductDetail/ProductDetail';
 
+
 // Public routes
 const publicRoutes = [
     { path: '/', component: Home, layout: DefaultLayout },
-    { path: '/login', component: Login, layout: DefaultLayout },
+    { path: '/login', component: Login, layout: AuthLayout },
+    { path: '/signup', component: Signup, layout: AuthLayout },
+    { path: '/shop/profile', component: UpdateShop, layout: SellerLayout },
     { path: '/product/new', component: NewProduct, layout: SellerLayout },
     { path: '/product/list/all', component: ListProduct, layout: SellerLayout },
-    { path: '/shop/profile', component: Updateshop, layout: SellerLayout },
-    { path: '/signinup', component: Signinup, layout: DefaultLayout },
     { path: '/product/detail', component: ProductDetail, layout: DefaultLayout },
 ];
 
