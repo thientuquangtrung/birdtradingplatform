6e34d846d77f0fb5e383fda7fc6b68edd9540870
Duc Nghia <ducnghia30703@gmail.com>
2023-05-22 07:04:35 +0700

    login

--- client/src/pages/Home/Login.js
+++ client/src/pages/Home/Login.js
@@ -1,36 +1,54 @@
-import React from 'react'
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import { Avatar, Button, Checkbox, FormControlLabel, Link, Paper, TextField, Typography } from '@mui/material';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import PersonIcon from '@mui/icons-material/Person';
 
-const Login = () => {
-    const paperStyle={padding: 20, height: '70vh',width:600,margin:"20px auto"}
-    const avatarStyle={backgroundColor:"lightblue"}
-    const marginStyle={margin:"10px 0"}
-  return (
-    <div>
-        <Grid>
-            <Paper elevation={5} style={paperStyle}>
-            <Grid align="center"> 
-                <Avatar style={avatarStyle}><LockOpenIcon></LockOpenIcon></Avatar>
-                <h2>Sign In</h2>
+const Login = ({ handleChange }) => {
+    // Destructure the handleChange prop
+    const paperStyle = { padding: 20, height: '66vh', width: 600, margin: '20px auto' };
+    const avatarStyle = { backgroundColor: 'lightblue' };
+    const marginStyle = { margin: '10px 0' };
+
+    return (
+        <div>
+            <Grid>
+                <Paper elevation={5} style={paperStyle}>
+                    <Grid align="center">
+                        <Avatar style={avatarStyle}>
+                            <LockOpenIcon />
+                        </Avatar>
+                        <h1>Sign In</h1>
+                    </Grid>
+                    <PersonIcon></PersonIcon>{' '}
+                    <TextField style={marginStyle} label="Email" placeholder="Enter username" fullWidth required />
+                    <TextField
+                        style={marginStyle}
+                        label="Password"
+                        type="password"
+                        placeholder="Enter password"
+                        fullWidth
+                        required
+                    />
+                    <Grid style={{display:"flex", justifyContent:"space-between"}}>
+                        <FormControlLabel control={<Checkbox defaultChecked />} label="Remember password" />
+                        <Typography style={marginStyle}>
+                            <Link href="#">Forgot password ?</Link>
+                        </Typography>
+                    </Grid>
+                    <Button variant="contained" type="submit" color="primary" fullWidth>
+                        Sign In
+                    </Button>
+                    <Typography>
+                        Do you have an account ?
+                        <Link href="#" onClick={() => handleChange('event', 1)}>
+                            Sign up
+                        </Link>
+                    </Typography>
+                </Paper>
             </Grid>
-            <TextField style={marginStyle} label="Username" placeholder='Enter username' fullWidth required></TextField>
-            <TextField style={marginStyle} label="Password" type='password' placeholder='Enter password' fullWidth required></TextField>
-            <FormControlLabel control={<Checkbox defaultChecked />} label="Remember password" />
-            <Button variant='contained' type="submit" color='primary' fullWidth> Sign In</Button>
-            <Typography style={marginStyle}>
-                <Link href="#">Forgot password ?</Link>
-            </Typography>
-            <Typography>
-                Do you have an account ?
-                <Link href="#">Sign up</Link>
-            </Typography>
-            </Paper>
-            
-        </Grid>
-    </div>
-  )
-}
+        </div>
+    );
+};
 
-export default Login
+export default Login;
