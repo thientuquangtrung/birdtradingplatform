da8d62204f3855df6934ec852c755b52577e3e84
ngantvh <vohongngan17022003@gmail.com>
2023-05-23 18:47:25 +0700

    New Product

--- server/src/api/controllers/productController.js
+++ server/src/api/controllers/productController.js
@@ -1,67 +1,66 @@
-const createError = require("http-errors");
-const productData = require('../services/product')
+const createError = require('http-errors');
+const productData = require('../services/product');
 
-const getProducts = async(req, res, next) => {
+const getProducts = async (req, res, next) => {
     try {
         const list = await productData.getProducts();
 
         list.map((product) => {
-            product.image = `${process.env.HOST_URL}/product/${product.image}`
-        })
+            product.image = `${process.env.HOST_URL}/product/${product.image}`;
+        });
 
-        return res.send(list)
-    }
-    catch(error) {   
+        return res.send(list);
+    } catch (error) {
         next(createError(error.status, error.message));
     }
-}
+};
 
 const getProductById = async (req, res, next) => {
     try {
-        const product = await productData.getProductById(req.params.id)
+        const product = await productData.getProductById(req.params.id);
 
-        product.image = `${process.env.HOST_URL}/product/${product.image}`
-    
-        return res.send(product)
+        product.image = `${process.env.HOST_URL}/product/${product.image}`;
+
+        return res.send(product);
     } catch (error) {
         next(createError(error.status, error.message));
     }
-} 
+};
 
 const getProductsOfSeller = async (req, res, next) => {
     try {
-        const list = await productData.getProductsOfSeller(req.payload.id)
+        const list = await productData.getProductsOfSeller(req.payload.id);
 
         list.map((product) => {
-            product.image = `${process.env.HOST_URL}/product/${product.image}`
-        })
+            product.image = `${process.env.HOST_URL}/product/${product.image}`;
+        });
 
-        return res.send(list)
+        return res.send(list);
     } catch (error) {
         next(createError(error.status, error.message));
     }
-}
+};
 
 const createProduct = async (req, res, next) => {
     try {
         const data = {
             ...req.body,
             image: req.file.filename,
-        }
-        const created = await productData.createProduct(data)
+        };
+        const created = await productData.createProduct(data);
 
         return res.send({
             ...created,
-            image: `${process.env.HOST_URL}/product/${req.file.filename}`
-        })
+            image: `${process.env.HOST_URL}/product/${req.file.filename}`,
+        });
     } catch (error) {
-        next(createError(error.status, error.message));
+        next(createError(error.message));
     }
-}
+};
 
 module.exports = {
     getProducts,
     getProductById,
     getProductsOfSeller,
-    createProduct
-}
+    createProduct,
+};
