7af221f43693068b09a2d75643eca5acc2550888
ngantvh <vohongngan17022003@gmail.com>
2023-05-25 11:27:56 +0700

    Table Product & Product Detail

--- client/src/components/ProductTable.js
+++ client/src/components/ProductTable.js
@@ -2,44 +2,57 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'Số thứ tự', width: 90 },
-  { field: 'name', headerName: 'Tên sản phẩm', width: 180 },
-  { field: 'price', headerName: 'Giá', width: 150 },
-  { field: 'sale', headerName: 'Doanh số', width: 130 },
-  {
-    field: 'status',
-    headerName: 'Thao tác',
-    width: 150,
-  },
-
+    { field: 'id', headerName: 'STT', width: 50 },
+    {
+        field: 'image',
+        headerName: 'Hình ảnh',
+        renderCell: (rowData) => <img src={rowData.value} style={{ width: '100%' }} />,
+    },
+    { field: 'name', headerName: 'Tên sản phẩm', width: 180 },
+    { field: 'price', headerName: 'Giá', width: 100 },
+    { field: 'description', headerName: 'Mô tả', width: 190 },
+    {
+        field: 'action',
+        headerName: 'Thao tác',
+        width: 100,
+    },
 ];
 
-const rows = [
-  { id: 1, name: 'bird', sale: '0', price: '100.000', status: 'Cập nhật' },
-  { id: 2, name: 'bird', sale: '0', price: '200.000', status: 'Cập nhật' },
-  { id: 3, name: 'bird', sale: '0', price: '150.000', status: 'Cập nhật' },
-  { id: 4, name: 'bird', sale: '0', price: '180.000', status: 'Cập nhật' },
-  { id: 5, name: 'bird', sale: '0', price: '120.000', status: 'Cập nhật' },
-  { id: 6, name: 'bird', sale: '0', price: '0', status: 'Cập nhật' },
-  { id: 7, name: 'bird', sale: '0', price: '300.000', status: 'Cập nhật' },
-  { id: 8, name: 'bird', sale: '0', price: '270.000', status: 'Cập nhật' },
-  { id: 9, name: 'bird', sale: '0', price: '50.000', status: 'Cập nhật' },
-];
+// const rows = [
+//   { id: 1, name: 'bird', sale: '0', price: '100.000', status: 'Cập nhật' },
+//   { id: 2, name: 'bird', sale: '0', price: '200.000', status: 'Cập nhật' },
+//   { id: 3, name: 'bird', sale: '0', price: '150.000', status: 'Cập nhật' },
+//   { id: 4, name: 'bird', sale: '0', price: '180.000', status: 'Cập nhật' },
+//   { id: 5, name: 'bird', sale: '0', price: '120.000', status: 'Cập nhật' },
+//   { id: 6, name: 'bird', sale: '0', price: '0', status: 'Cập nhật' },
+//   { id: 7, name: 'bird', sale: '0', price: '300.000', status: 'Cập nhật' },
+//   { id: 8, name: 'bird', sale: '0', price: '270.000', status: 'Cập nhật' },
+//   { id: 9, name: 'bird', sale: '0', price: '50.000', status: 'Cập nhật' },
+// ];
 
-export default function ProductTable() {
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
-    </div>
-  );
+export default function ProductTable({ rows = [] }) {
+    if (rows.length > 0) {
+        rows.map((row) => {
+            delete row.shopId;
+            delete row.categoryId;
+
+            row.action = 'Cập nhật';
+        });
+    }
+
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+            />
+        </div>
+    );
 }
