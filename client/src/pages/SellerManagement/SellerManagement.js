36667007b99662985f21c24d129d35cf86bbd33a
Trung Nguyen <thientuquangtrung@gmail.com>
2023-06-08 19:59:34 +0700

    admin layout

---
+++ client/src/pages/SellerManagement/SellerManagement.js
@@ -0,0 +1,28 @@
+import { Paper, Stack, Typography } from '@mui/material';
+import { Button, Input } from '@mui/joy';
+import { Add, Search } from '@mui/icons-material';
+
+function SellerManagement() {
+    return (
+        <Stack direction="column" padding={5} gap={5}>
+            <Stack direction="row" justifyContent="space-between" alignItems="center">
+                <Typography variant="h4" component="h2">
+                    Sellers
+                </Typography>
+                <Button variant="soft" color="primary" startDecorator={<Add />}>
+                    Add
+                </Button>
+            </Stack>
+            <Paper elevation={2} sx={{ height: '80px', padding: 2 }}>
+                <Input
+                    startDecorator={<Search />}
+                    sx={{ width: '50%', height: '100%' }}
+                    placeholder="Search for account..."
+                />
+            </Paper>
+            <Paper elevation={2} sx={{ height: '500px', padding: 2 }}></Paper>
+        </Stack>
+    );
+}
+
+export default SellerManagement;
