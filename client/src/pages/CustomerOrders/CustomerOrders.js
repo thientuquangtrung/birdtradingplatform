d11455e306751c004ebe7e99a771155aefff62dc
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2023-06-09 13:35:03 +0700

    design feedback modal and feedback list of product

--- client/src/pages/CustomerOrders/CustomerOrders.js
+++ client/src/pages/CustomerOrders/CustomerOrders.js
@@ -3,16 +3,62 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import { Paper } from '@mui/material';
+import { Grid, ImageList, ImageListItem, Paper, Stack, TextareaAutosize } from '@mui/material';
 import { useState } from 'react';
 import SubCustomerLayout from '../../layouts/SubCustomerLayout/SubCustomerLayout';
 
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { styled } from '@mui/material/styles';
+import UploadImage from '../../components/UploadImage';
+
 function CustomerOrders() {
+    const [open, setOpen] = useState(false);
+    const [upLoadFile, setUpLoadFile] = useState('');
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => {
+        setOpen(false);
+        // Thêm các xử lý khác nếu cần thiết để quay về trang ban đầu
+    };
+
     const [value, setValue] = useState('1');
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 600,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageSelect = (event) => {
+        const file = event.target.files[0];
+        setSelectedImage(URL.createObjectURL(file));
+    };
+
+    const ImageContainer = styled('div')`
+        width: 80px;
+        height: 80px;
+        overflow: hidden;
+    `;
+
+    const StyledImage = styled('img')`
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    `;
 
     return (
         <SubCustomerLayout>
@@ -33,6 +79,109 @@ function CustomerOrders() {
                     <TabPanel value="4">Item Four</TabPanel>
                     <TabPanel value="5">Item Five</TabPanel>
                 </TabContext>
+
+                <div>
+                    <Button onClick={handleOpen}>Đánh Giá</Button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <Typography sx={{ paddingLeft: 1, paddingBottom: 2 }} id="modal-modal-title" variant="h4">
+                                Đánh Giá Sản Phẩm
+                            </Typography>
+                            <Box
+                                sx={{
+                                    m: 1,
+                                    width: 510,
+                                }}
+                            >
+                                <Paper variant="outlined" sx={{ backgroundColor: '#f5f5f5' }}>
+                                    <Stack direction="column" sx={{ paddingBottom: 3 }}>
+                                        <Paper
+                                            variant="outlined"
+                                            sx={{
+                                                backgroundColor: 'white',
+                                                m: 3,
+                                                width: 460,
+                                                height: 250,
+                                                fontSize: 'small',
+                                            }}
+                                        >
+                                            <TextareaAutosize
+                                                // value={description}
+                                                // onChange={(e) => setDescription(e.target.value)}
+                                                minRows={3}
+                                                placeholder="Nhập đánh giá sản phẩm"
+                                                style={{
+                                                    padding: 10,
+                                                    height: '100%',
+                                                    width: '100%',
+                                                    fontSize: 15,
+                                                    fontFamily: 'Roboto',
+                                                }}
+                                            />
+                                        </Paper>
+                                        {/* <Stack
+                                            flexGrow={1}
+                                            direction="row"
+                                            alignItems="center"
+                                            justifyContent="center"
+                                            spacing={2}
+                                        >
+                                            <input
+                                                accept="image/*"
+                                                type="file"
+                                                style={{ display: 'none' }}
+                                                onChange={handleImageSelect}
+                                                id="image-input"
+                                                required
+                                            />
+                                            <label
+                                                htmlFor="image-input"
+                                                style={{
+                                                    display: 'flex',
+                                                    alignItems: 'center',
+                                                    justifyContent: 'center',
+                                                }}
+                                            >
+                                                <Button component="span" variant="outlined" size="small">
+                                                    Thêm Hình Ảnh
+                                                </Button>
+                                            </label>
+                                            <ImageContainer
+                                                style={{ display: 'flex', alignItems: 'center', marginRight: 15 }}
+                                            >
+                                                {selectedImage && <StyledImage src={selectedImage} alt="Selected" />}
+                                            </ImageContainer>
+                                        </Stack> */}
+                                        <UploadImage
+                                            title="Select a logo"
+                                            uploadFile={upLoadFile}
+                                            setUploadFile={setUpLoadFile}
+                                        />
+                                    </Stack>
+                                </Paper>
+                            </Box>
+                            <Stack
+                                justifyContent="flex-end"
+                                spacing={1}
+                                direction="row"
+                                sx={{ marginRight: 1.8, paddingTop: 1 }}
+                            >
+                                <Button size="small" variant="text" onClick={handleClose}>
+                                    Trở Lại
+                                </Button>
+
+                                <Button size="small" variant="contained" onClick={handleClose}>
+                                    Hoàn Thành
+                                </Button>
+                            </Stack>
+                        </Box>
+                    </Modal>
+                </div>
             </Paper>
         </SubCustomerLayout>
     );
--- client/src/pages/ProductDetail/ProductDetail.js
+++ client/src/pages/ProductDetail/ProductDetail.js
@@ -12,6 +12,7 @@ import handleError from '../../utils/handleError';
 import AuthContext from '../../contexts/AuthContext';
 import { enqueueSnackbar } from 'notistack';
 import CartContext from '../../contexts/CartContext';
+import { Grid, Avatar, ImageList, ImageListItem } from '@mui/material';
 
 function ProductDetail() {
     const navigate = useNavigate();
@@ -20,6 +21,13 @@ function ProductDetail() {
     const location = useLocation();
     const [product, setProduct] = useState('');
     const [quantity, setQuantity] = useState(1);
+
+    const itemData = [
+        {
+            img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+            title: 'Breakfast',
+        },
+    ];
 
     useEffect(() => {
         axiosClient
@@ -60,44 +68,134 @@ function ProductDetail() {
     }
 
     return (
-        <Paper sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Box sx={{ padding: 2 }}>
-                <img style={{ width: '500px', height: '500px', objectFit: 'contain' }} src={product.image} alt="Bird" />
-            </Box>
-            <Box sx={{ padding: 4, width: '70%' }}>
-                <Stack direction="column" gap={5}>
-                    <Typography variant="h5" gutterBottom fontWeight={'bold'}>
-                        {product.name}
-                    </Typography>
-                    <Typography variant="h4" gutterBottom color="#c80606">
-                        {product.price ? product.price.toLocaleString('vi-VN') : ''}
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom sx={{ fontSize: 16 }}>
-                        {product.description}
-                    </Typography>
-
-                    <TextField
-                        value={quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
-                        defaultValue="1"
-                        type="number"
-                        inputProps={{ min: '0', max: '10', step: '1' }}
-                        sx={{ width: '20%' }}
+        <div>
+            <Paper sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: 3 }}>
+                <Box sx={{ padding: 2 }}>
+                    <img
+                        style={{ width: '500px', height: '500px', objectFit: 'contain' }}
+                        src={product.image}
+                        alt="Bird"
                     />
+                </Box>
+                <Box sx={{ padding: 4, width: '70%' }}>
+                    <Stack direction="column" gap={5}>
+                        <Typography variant="h5" gutterBottom fontWeight={'bold'}>
+                            {product.name}
+                        </Typography>
+                        <Typography variant="h4" gutterBottom color="#c80606">
+                            {product.price ? product.price.toLocaleString('vi-VN') : ''}
+                        </Typography>
+                        <Typography variant="subtitle2" gutterBottom sx={{ fontSize: 16 }}>
+                            {product.description}
+                        </Typography>
 
-                    <Box>
-                        <Button
-                            onClick={handleAddToCart}
-                            variant="contained"
-                            startIcon={<AddShoppingCartIcon />}
-                            sx={{ color: '#fff', backgroundColor: '#1976d2' }}
-                        >
-                            Thêm vào giỏ hàng
-                        </Button>
-                    </Box>
-                </Stack>
-            </Box>
-        </Paper>
+                        <TextField
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                            defaultValue="1"
+                            type="number"
+                            inputProps={{ min: '0', max: '10', step: '1' }}
+                            sx={{ width: '20%' }}
+                        />
+
+                        <Box>
+                            <Button
+                                onClick={handleAddToCart}
+                                variant="contained"
+                                startIcon={<AddShoppingCartIcon />}
+                                sx={{ color: '#fff', backgroundColor: '#1976d2' }}
+                            >
+                                Thêm vào giỏ hàng
+                            </Button>
+                        </Box>
+                    </Stack>
+                </Box>
+            </Paper>
+
+            <Paper elevation={1} sx={{ padding: 3 }}>
+                <Typography sx={{ paddingBottom: 2 }} variant="h6" gutterBottom>
+                    ĐÁNH GIÁ SẢN PHẨM
+                </Typography>
+                <Grid container sx={{ paddingBottom: 3, borderBottom: '1px solid #e0e0e0', justifyContent: 'center' }}>
+                    <Grid item xs={1} sx={{ paddingLeft: 2 }}>
+                        <Avatar
+                            alt="Remy Sharp"
+                            sx={{ width: 60, height: 60 }}
+                            src="https://cafebiz.cafebizcdn.vn/thumb_w/600/162123310254002176/2021/4/22/photo1619081646120-16190816462552046046483.jpg"
+                        />
+                    </Grid>
+                    <Grid item xs={11}>
+                        <Stack>
+                            <Typography variant="h6" display="block">
+                                Taylor Swift
+                            </Typography>
+                            <Typography
+                                variant="caption"
+                                display="block"
+                                gutterBottom
+                                sx={{ color: 'grey', fontSize: '10px' }}
+                            >
+                                11/2/23 4:57
+                            </Typography>
+                            <Typography variant="body2" gutterBottom sx={{ paddingTop: 1.5, wordWrap: 'break-word' }}>
+                                zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+                            </Typography>
+                            <ImageList sx={{ width: 300, paddingTop: 1 }}>
+                                {itemData.map((item) => (
+                                    <ImageListItem key={item.img}>
+                                        <img
+                                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                            alt={item.title}
+                                        />
+                                    </ImageListItem>
+                                ))}
+                            </ImageList>
+                        </Stack>
+                    </Grid>
+                </Grid>
+
+                <Grid container sx={{ paddingTop: 3 }}>
+                    <Grid item xs={1} sx={{ paddingLeft: 2 }}>
+                        <Avatar
+                            alt="Remy Sharp"
+                            sx={{ width: 56, height: 56 }}
+                            src="https://cafebiz.cafebizcdn.vn/thumb_w/600/162123310254002176/2021/4/22/photo1619081646120-16190816462552046046483.jpg"
+                        />
+                    </Grid>
+                    <Grid item xs={11}>
+                        <Stack>
+                            <Typography variant="h6" display="block">
+                                Taylor Swift
+                            </Typography>
+                            <Typography
+                                variant="caption"
+                                display="block"
+                                gutterBottom
+                                sx={{ color: 'grey', fontSize: '10px' }}
+                            >
+                                11/2/23 4:57
+                            </Typography>
+                            <Typography variant="body2" gutterBottom sx={{ paddingTop: 1, wordWrap: 'break-word' }}>
+                                Sản phẩm đẹp, giá hợp lí
+                                zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+                            </Typography>
+                            <ImageList sx={{ width: 300 }}>
+                                {itemData.map((item) => (
+                                    <ImageListItem key={item.img}>
+                                        <img
+                                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                            alt={item.title}
+                                        />
+                                    </ImageListItem>
+                                ))}
+                            </ImageList>
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
     );
 }
 
