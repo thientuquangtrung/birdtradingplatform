f7d4090f44e47cad0ab387fb3cd67903e7cf3549
Trung Nguyen <thientuquangtrung@gmail.com>
2023-06-10 18:18:58 +0700

    api sprint 3

---
+++ server/src/api/services/feedback/getFeedBacksOfProduct.sql
@@ -0,0 +1,5 @@
+SELECT f.*, oh.customerId, oh.shopId, od.productId
+FROM [Feedback] f
+JOIN [OrderHeader] oh ON f.orderHeaderId = oh.id
+JOIN [OrderDetail] od ON oh.id = od.orderHeaderId
+WHERE od.productId = ''
