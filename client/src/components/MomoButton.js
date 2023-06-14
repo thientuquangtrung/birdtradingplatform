7c8213e95b773d0151f5a35fb3f687736484da2a
Trung Nguyen <thientuquangtrung@gmail.com>
2023-06-14 23:49:45 +0700

    fix momo button

--- client/src/components/MomoButton.js
+++ client/src/components/MomoButton.js
@@ -1,13 +1,22 @@
 import Button from '@mui/joy/Button';
 import { Typography } from '@mui/material';
+import axiosClient from '../api/axiosClient';
 
 function MoMoButton() {
-    return ( 
-        <Button color="danger" variant="soft"  sx={{width: '280px'}}>
+    const handlePayment = () => {
+        axiosClient
+            .post('create_momo_payment')
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error));
+    };
+    return (
+        <Button onClick={handlePayment} color="danger" variant="soft" sx={{ width: '280px' }}>
             <img width={50} height={50} src="https://warburgpincus.com/wp-content/uploads/2018/10/MoMo_logo-1.png" />
-            <Typography variant='h6' fontSize={16} color='#cb278b'>Thanh toán bằng Ví MoMo</Typography>
+            <Typography variant="h6" fontSize={16} color="#cb278b">
+                Thanh toán bằng Ví MoMo
+            </Typography>
         </Button>
-     );
+    );
 }
 
 export default MoMoButton;
